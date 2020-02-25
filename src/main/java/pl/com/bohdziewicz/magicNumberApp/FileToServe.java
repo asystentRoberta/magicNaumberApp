148fc93de980e0ce61853091dacd1c7c067cc6ed
AsystentRoberta <asystent.roberta@gmail.com>
2020-02-25 12:50:02 +0100

    Preparation for file handling

---
+++ src/main/java/pl/com/bohdziewicz/magicNumberApp/FileToServe.java
@@ -0,0 +1,27 @@
+package pl.com.bohdziewicz.magicNumberApp;
+
+import java.io.File;
+
+class FileToServe {
+
+    private File file;
+    private int sizeInBytes;
+    private String fileName;
+    private String extensionOfFile;
+
+    FileToServe(File file) {
+
+        this.file = file;
+    }
+
+    String getExtensionOfFile() {
+
+        fileName = file.getName();
+        return fileName.substring(fileName.length() - 3);
+    }
+
+    int getSizeInBytes() {
+
+        return (int) file.length();
+    }
+}
