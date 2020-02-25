a60f30c38dbe2efe2b740553059a9bc3aa273cc4
AsystentRoberta <asystent.roberta@gmail.com>
2020-02-25 13:19:53 +0100

    Reader to read all bytes from file and first-n bytes from file

--- src/main/java/pl/com/bohdziewicz/magicNumberApp/GifTester.java
+++ src/main/java/pl/com/bohdziewicz/magicNumberApp/GifTester.java
@@ -1,9 +1,19 @@
 package pl.com.bohdziewicz.magicNumberApp;
 
+import java.util.Arrays;
+
 public class GifTester implements FileTesterInterface {
+
+    //Hex signature -> Dec signature (using BYTE) (for example FF->-1 or 47->71
+    private static final byte[] gifFirstSixBytesFirstSignature = { 71, 73, 70, 56, 55, 97 };
+    private static final byte[] gifFirstSixBytesSecondSinature = { 71, 73, 70, 56, 57, 97 };
 
     @Override public boolean isFileHasCorrectExtension(FileToServe fileToServe) {
 
-        return false;
+        FileBytesReader fileBytesReader = new FileBytesReader();
+        final byte[] sixFirstBytesOfTestedFile = fileBytesReader.readNFirstBytesFromFile(fileToServe, 6);
+
+        return Arrays.equals(gifFirstSixBytesFirstSignature, sixFirstBytesOfTestedFile) || Arrays
+                .equals(gifFirstSixBytesSecondSinature, sixFirstBytesOfTestedFile);
     }
 }
